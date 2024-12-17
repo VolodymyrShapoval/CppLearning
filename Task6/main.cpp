e6987bb5186a3d1783f2e1bf80c4b350e2f01e68
VolodymyrShapoval <volodymyr.sh.05@gmail.com>
2024-12-17 18:35:04 +0200

    task6: remake of task5 via references

---
+++ Task6/main.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <stdexcept>
+
+using namespace std;
+
+void Swap(int &a, int &b)
+{
+	int temp = a;
+	a = b;
+	b = temp;
+}
+
+void main()
+{
+	int a = 5, b = 10;
+	cout << "Before swap:" << endl;
+	cout << "a: " << a << endl;
+	cout << "b: " << b << endl;
+	try
+	{
+		cout << "After swap: " << endl;
+		Swap(a, b);
+		cout << "a: " << a << endl;
+		cout << "b: " << b << endl;
+	}
+	catch (exception& ex)
+	{
+		cout << "Error: " << ex.what() << endl;
+	}
+}
