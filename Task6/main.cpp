2a8196776ad468671596fcbce2e8e342a4b390bc
VolodymyrShapoval <volodymyr.sh.05@gmail.com>
2024-12-30 20:22:00 +0200

    task6: did homework for simplecode using references and template values

--- Task6/main.cpp
+++ Task6/main.cpp
@@ -3,25 +3,36 @@
 
 using namespace std;
 
-void Swap(int &a, int &b)
+template <typename T>
+void Swap(T &a, T &b)
 {
-	int temp = a;
+	T temp = a;
 	a = b;
 	b = temp;
 }
 
 void main()
 {
-	int a = 5, b = 10;
+	int a = 5;
+	int b = 10;
+	string str1 = "Hello";
+	string str2 = "World";
 	cout << "Before swap:" << endl;
 	cout << "a: " << a << endl;
 	cout << "b: " << b << endl;
+	cout << "Before swap:" << endl;
+	cout << "str1: " << str1 << endl;
+	cout << "str2: " << str2 << endl;
 	try
 	{
 		cout << "After swap: " << endl;
 		Swap(a, b);
 		cout << "a: " << a << endl;
 		cout << "b: " << b << endl;
+		cout << "After swap: " << endl;
+		Swap(str1, str2);
+		cout << "str1: " << str1 << endl;
+		cout << "str2: " << str2 << endl;
 	}
 	catch (exception& ex)
 	{
