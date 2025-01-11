a4ce91695c29e57df4b62f870784e08982c2f58d
VolodymyrShapoval <volodymyr.sh.05@gmail.com>
2025-01-12 01:25:50 +0200

    mystring: overloaded + operator, created copy constructor

--- MyString/main.cpp
+++ MyString/main.cpp
@@ -17,6 +17,19 @@ public:
 
 		this->str[length] = '\0';
 	}
+	MyString(const MyString& other)
+	{
+		int length = strlen(other.str);
+
+		this->str = new char[length + 1];
+
+		for (size_t i = 0; i < length; ++i)
+		{
+			this->str[i] = other.str[i];
+		}
+
+		this->str[length] = '\0';
+	}
 	~MyString()
 	{
 		delete[] this->str;
@@ -43,9 +56,31 @@ public:
 		return *this;
 	}
 
-	MyString& operator + (const MyString& other)
+
+
+	MyString operator + (const MyString& other)
 	{
-		
+		int myStrLength = strlen(this->str);
+		int otherStrLength = strlen(other.str);
+		int length = myStrLength + otherStrLength;
+
+		MyString new_str;
+		new_str.str = new char[length + 1];
+
+		for (size_t i = 0; i < myStrLength; ++i)
+		{
+			new_str.str[i] = this->str[i];
+		}
+
+		for (size_t i = myStrLength, j = 0; i < length; ++i, ++j)
+		{
+			new_str.str[i] = other.str[j];
+		}
+
+		new_str.str[length] = '\0';
+
+
+		return new_str;
 	}
 
 	void print()
@@ -62,7 +97,11 @@ int main()
 {
 	try
 	{
-		MyString str = "Hello";
+		MyString str1 = "Hello";
+		MyString str2 = "World";
+
+		MyString str = str1 + str2;
+
 		str.print();
 	}
 	catch (std::exception& ex)
