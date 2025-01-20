47588c19469cbd347dff1b4e72743f31f97f4027
VolodymyrShapoval <volodymyr.sh.05@gmail.com>
2025-01-20 19:00:14 +0200

    task13: added displayInfo methods

--- Task13/main.cpp
+++ Task13/main.cpp
@@ -5,36 +5,71 @@
 class Human
 {
 public:
-	Human(const std::size_t id, const std::string& name, const std::string& gender, const std::string& firstName, const std::string& lastName, const std::string& documentNumber, const std::size_t authorityNumber, const std::string& category, const std::string& city)
-		: id(id), name(name), gender(gender), drivingLicence(firstName, lastName, documentNumber, authorityNumber, category, city) {};
+	static size_t counter;
+
+	Human(const std::string& firstName, const std::string& lastName, const std::string& gender, const std::string& documentNumber, const std::string authorityNumber, const std::string& category, const std::string& city)
+		: m_firstName(firstName), m_lastName(lastName), m_gender(gender), m_drivingLicence(documentNumber, authorityNumber, category, city)
+	{
+		++counter;
+		this->m_id = counter;
+	};
+
+	void displayInfo()
+	{
+		std::cout << "User #" << this->m_id << std::endl;
+		std::cout << "First name: " << this->m_firstName << std::endl;
+		std::cout << "Last name: " << this->m_lastName << std::endl;
+		std::cout << "Gender: " << this->m_gender << std::endl;
+		this->m_drivingLicence.displayInfo();
+	}
 
 private:
 	class DrivingLicence
 	{
 	public:
-		DrivingLicence(const std::string& firstName, const std::string& lastName, const std::string& documentNumber, const std::size_t authorityNumber, const std::string& category, const std::string& city)
-			: firstName(firstName), lastName(lastName), documentNumber(documentNumber), authorityNumber(authorityNumber), category(category), city(city) {}
+		DrivingLicence(const std::string& documentNumber, const std::string& authorityNumber, const std::string& category, const std::string& city)
+			: m_documentNumber(documentNumber), m_authorityNumber(authorityNumber), m_category(category), m_city(city) {}
 			
+		void displayInfo()
+		{
+			std::cout << "Driving licence #" << this->m_documentNumber << std::endl;
+			std::cout << "--> Authority number: " << this->m_authorityNumber << std::endl;
+			std::cout << "--> Category: " << this->m_category << std::endl;
+			std::cout << "--> City: " << this->m_city << std::endl;
+		}
+
 	private:
-		std::string firstName;
-		std::string lastName;
-		std::string documentNumber;
-		std::size_t	authorityNumber;
-		std::string category;
-		std::string city;
+		std::string m_documentNumber;
+		std::string	m_authorityNumber;
+		std::string m_category;
+		std::string m_city;
 	};
 
-	std::size_t		id;
-	std::string		name;
-	std::string		gender;
-	DrivingLicence	drivingLicence;
+	std::size_t		m_id;
+	std::string		m_firstName;
+	std::string		m_lastName;
+	std::string		m_gender;
+	DrivingLicence	m_drivingLicence;
 };
+
+std::size_t Human::counter = 0;
 
 int main()
 {
 	try
 	{
+		Human human1 =
+		{
+			"John",
+			"Doe",
+			"Male",
+			"29552886",
+			"2584",
+			"B",
+			"Kyiv"
+		};
 
+		human1.displayInfo();
 	}
 	catch (std::exception& ex)
 	{
