ff2fce65eb255f0188290d703dfbb93128e6e6fe
VolodymyrShapoval <volodymyr.sh.05@gmail.com>
2025-01-20 21:12:46 +0200

    task13: added objects array

--- Task13/main.cpp
+++ Task13/main.cpp
@@ -7,12 +7,17 @@ class Human
 public:
 	static size_t counter;
 
+	Human()
+	{
+		++counter;
+		this->m_id = counter;
+	}
 	Human(const std::string& firstName, const std::string& lastName, const std::string& gender, const std::string& documentNumber, const std::string authorityNumber, const std::string& category, const std::string& city)
 		: m_firstName(firstName), m_lastName(lastName), m_gender(gender), m_drivingLicence(documentNumber, authorityNumber, category, city)
 	{
 		++counter;
 		this->m_id = counter;
-	};
+	}
 
 	void displayInfo()
 	{
@@ -27,6 +32,7 @@ private:
 	class DrivingLicence
 	{
 	public:
+		DrivingLicence() = default;
 		DrivingLicence(const std::string& documentNumber, const std::string& authorityNumber, const std::string& category, const std::string& city)
 			: m_documentNumber(documentNumber), m_authorityNumber(authorityNumber), m_category(category), m_city(city) {}
 			
@@ -58,18 +64,24 @@ int main()
 {
 	try
 	{
-		Human human1 =
-		{
-			"John",
-			"Doe",
-			"Male",
-			"29552886",
-			"2584",
-			"B",
-			"Kyiv"
-		};
+		Human human1 = { "John", "Doe", "Male", "29552886", "2584", "B", "Kyiv" };
+		Human human2 = { "Jane", "Smith", "Female", "29552887", "2585", "A", "Lviv" };
+		Human human3 = { "Alex", "Johnson", "Male", "29552888", "2586", "O", "Odessa" };
+		Human human4 = { "Emily", "Davis", "Female", "29552889", "2587", "AB", "Kharkiv" };
+		Human human5 = { "Michael", "Brown", "Male", "29552890", "2588", "B", "Dnipro" };
 
-		human1.displayInfo();
+		const int LENGTH = 5;
+		Human humanArr[LENGTH] = { human1, human2, human3, human4, human5 };
+		
+		for (size_t i = 0; i < LENGTH; ++i)
+		{
+			humanArr[i].displayInfo();
+			std::cout << std::string(25, '-') << std::endl;
+		}
+
+		Human* humanDynamicArr = new Human[5];
+		delete[] humanDynamicArr;
+
 	}
 	catch (std::exception& ex)
 	{
