06ee76293a0a3c1bf96e298a971237534a6d1709
VolodymyrShapoval <volodymyr.sh.05@gmail.com>
2025-01-22 20:08:43 +0200

    file_task1: reorganised program, added menu, added oportunity to input data via console

--- FileTask1/main.cpp
+++ FileTask1/main.cpp
@@ -3,23 +3,53 @@
 #include <stdexcept>
 #include <fstream>
 
+void write_to_file(const std::string& PATH)
+{
+	if (PATH.empty())
+	{
+		throw std::invalid_argument("The file path is empty!");
+	}
+
+	std::ofstream fout;
+	fout.open(PATH, std::ios::app);
+
+	if (!fout.is_open())
+	{
+		throw std::ios_base::failure("Failed to open file at path " + PATH);
+	}
+
+	std::cout << "Input data: ";
+	std::string data;
+	std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+	std::getline(std::cin, data);
+	fout << data << std::endl;
+
+	fout.close();
+}
+
 int main()
 {
 	try
 	{
 		const std::string PATH = "myFile.txt";
-		std::ofstream fileOut;
-		fileOut.open(PATH, std::ios::app);
 
-		if (!fileOut.is_open())
+		std::cout << "1. Write to file\n"
+				<< "2. Read from file\n"
+				<< "0. Exit\n"
+				<< "Choose operation: ";
+		std::size_t choice;
+		std::cin >> choice;
+		switch (choice)
 		{
-			throw std::ios_base::failure("Failed to open file at path " + PATH);
+		case 1:
+			write_to_file(PATH);
+			break;
+		case 0:
+			std::cout << "Goodbye!" << std::endl;
+			break;
+		default:
+			break;
 		}
-
-		fileOut << "Working with \"fstream\" library." << std::endl;
-		fileOut << "Continue learning C++..." << std::endl;
-
-		fileOut.close();
 	}
 	catch (std::exception& ex)
 	{
