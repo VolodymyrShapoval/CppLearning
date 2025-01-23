ca7055c4bba2acd33ffd0f49d3a57791084d3068
VolodymyrShapoval <volodymyr.sh.05@gmail.com>
2025-01-23 20:11:38 +0200

    file_task1: added object writer and reader using template functions

--- FileTask1/main.cpp
+++ FileTask1/main.cpp
@@ -3,6 +3,22 @@
 #include <stdexcept>
 #include <fstream>
 
+class Human
+{
+public:
+	Human() = default;
+	Human(const std::string& name, const std::size_t age) : m_name(name), m_age(age) {}
+	void display_info()
+	{
+		std::cout << "Name: " << this->m_name << std::endl;
+		std::cout << "Age: " << this->m_age << std::endl;
+	}
+
+private:
+	std::string m_name;
+	std::size_t m_age;
+};
+
 void write_to_file(const std::string& PATH)
 {
 	if (PATH.empty())
@@ -23,6 +39,27 @@ void write_to_file(const std::string& PATH)
 	std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
 	std::getline(std::cin, data);
 	fout << data << std::endl;
+
+	fout.close();
+}
+
+template<class T>
+void write_to_file(const T& object, const std::string& PATH)
+{
+	if (PATH.empty())
+	{
+		throw std::invalid_argument("The file path is empty!");
+	}
+
+	std::ofstream fout;
+	fout.open(PATH, std::ios::app);
+
+	if (!fout.is_open())
+	{
+		throw std::ios_base::failure("Failed to open file at path " + PATH);
+	}
+
+	fout.write((char*)&object, sizeof(T));
 
 	fout.close();
 }
@@ -52,26 +89,75 @@ void read_from_file(const std::string& PATH)
 	fin.close();
 }
 
+template<class T>
+void read_from_file(T& object, const std::string& PATH)
+{
+	if (PATH.empty())
+	{
+		throw std::invalid_argument("The file path is empty!");
+	}
+
+	std::ifstream fin;
+	fin.open(PATH);
+
+	if (!fin.is_open())
+	{
+		throw std::ios_base::failure("Failed to open file at path " + PATH);
+	}
+
+	while (fin.read((char*)&object, sizeof(T)))
+	{
+		
+	}
+	fin.close();
+}
+
 int main()
 {
 	try
 	{
-		const std::string PATH = "myFile.txt";
+		const std::string textPATH = "txtFile.txt";
+		const std::string objectPATH = "objectFile.txt";
 
-		std::cout << "1. Write to file\n"
-				<< "2. Read from file\n"
+		std::cout << "1. Write string to file\n"
+				<< "2. Write object to file\n"
+				<< "3. Read string from file\n"
+				<< "4. Read object from file\n"
 				<< "0. Exit\n"
 				<< "Choose operation: ";
-		std::size_t choice;
+		int choice;
 		std::cin >> choice;
 		switch (choice)
 		{
 		case 1:
-			write_to_file(PATH);
+		{
+			write_to_file(textPATH);
 			break;
+		}
 		case 2:
-			read_from_file(PATH);
+		{
+			std::string name;
+			std::size_t age;
+
+			std::cout << "Input name: ";
+			std::cin >> name;
+			std::cout << "Input age: ";
+			std::cin >> age;
+
+			Human human = Human(name, age);
+			write_to_file(human, objectPATH);	
 			break;
+		}
+		case 3:
+			read_from_file(textPATH);
+			break;
+		case 4:
+		{
+			Human human;
+			read_from_file(human, objectPATH);
+			human.display_info();
+			break;
+		}
 		case 0:
 			std::cout << "Goodbye!" << std::endl;
 			break;
