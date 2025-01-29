463558079191f97cedb1481f9534af8c80832f47
VolodymyrShapoval <volodymyr.sh.05@gmail.com>
2025-01-29 21:42:49 +0200

    linked_list: transported realisation of LinkedList and Node methods to .h files because of impossibility of defining template methods in .cpp files

--- LinkedList/Node.h
+++ LinkedList/Node.h
@@ -15,5 +15,20 @@ public:
 	Node(const Node<T>& node);
 };
 
+
+template <class T>
+Node<T>::Node(const T& value, Node<T>* next)
+{
+	this->value = value;
+	this->next = next;
+}
+
+template <class T>
+Node<T>::Node(const Node<T>& node)
+{
+	this->value = node.value;
+	this->next = node.next;
+}
+
 #endif // !_NODE_H_
 
