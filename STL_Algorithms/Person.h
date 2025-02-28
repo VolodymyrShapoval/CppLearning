98b757eec0be62d099513304459c2d12d846c4ce
VolodymyrShapoval <volodymyr.sh.05@gmail.com>
2025-02-28 12:55:38 +0200

    stl_algorithms: added predicate using into std::equal and std::mismatch functions

---
+++ STL_Algorithms/Person.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#include <iostream>
+#include <string>
+
+struct Person
+{
+public:
+	std::string name;
+	std::int16_t age;
+};
+
--- STL_Algorithms/main.cpp
+++ STL_Algorithms/main.cpp
@@ -5,19 +5,39 @@
 #include <list>
 #include <algorithm>
 #include <numeric>
+#include "Person.h"
 
+bool areAgesEqual(const Person& person1, const Person& person2)
+{
+	return person1.age == person2.age;
+}
 
 int main()
 {
 	try
 	{
-		std::vector<int> myVec1{ 53, 4, 22, 12, 61, 3, 72 };
-		std::vector<int> myVec2{ 53, 4, 22, 12, 61, 3, 72 };
-		bool result = std::equal(std::begin(myVec1), std::end(myVec1), std::begin(myVec2), std::end(myVec2));
-		auto pairRes = std::mismatch(std::begin(myVec1), std::end(myVec1), std::begin(myVec2), std::end(myVec2));
+		std::vector<Person> myVec1
+		{
+			{"Alice", 25},
+			{"Bob", 30},
+			{"Charlie", 22},
+			{"David", 35},
+			{"Emma", 28}
+		};
+		std::vector<Person> myVec2
+		{
+			{"Alice", 25},
+			{"Bob", 30},
+			{"Charlie", 21},
+			{"David", 35},
+			{"Emma", 28}
+		};
+
+		bool result = std::equal(std::begin(myVec1), std::end(myVec1), std::begin(myVec2), std::end(myVec2), areAgesEqual);
+		auto pairRes = std::mismatch(std::begin(myVec1), std::end(myVec1), std::begin(myVec2), std::end(myVec2), areAgesEqual);
 		std::cout << std::boolalpha << "Result(equal): " << result << std::endl;
-		std::cout << std::boolalpha << "Result(mismatch): " << *pairRes.first << std::endl;
-		std::cout << std::boolalpha << "Result(mismatch): " << *pairRes.second << std::endl;
+		std::cout << std::boolalpha << "Result(mismatch): " << (*pairRes.first).name << " - " << (*pairRes.first).age << std::endl;
+		std::cout << std::boolalpha << "Result(mismatch): " << (*pairRes.second).name << " - " << (*pairRes.second).age << std::endl;
 	}
 	catch (const std::exception ex)
 	{
